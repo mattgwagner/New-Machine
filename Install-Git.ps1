01862d1c8ca236c3ec36192be655c0581ba30a6c
Matt Wagner <mattgwagner@gmail.com>
2013-12-31 19:03:56 -0500

    Add powershell profile creation

--- Install-Git.ps1
+++ Install-Git.ps1
@@ -11,3 +11,12 @@ cinst githubforwindows
 cinst TortoiseGit
 
 cinst poshgit
+
+## Create a new PowerShell Profile
+if (!(test-path $profile)) 
+{
+    New-Item -path $profile -type file -force
+}
+
+## Add the GitHub profile shell
+Add-Content $profile ". (Resolve-Path '$env:LOCALAPPDATA\GitHub\shell.ps1')"
