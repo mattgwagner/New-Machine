e15a7b81ba5e0e13b7446be433686e42e0e8302f
Matt Wagner <mattgwagner@gmail.com>
2014-02-09 11:13:06 -0600

    Tweak message on start-up

--- Functions.psm1
+++ Functions.psm1
@@ -7,7 +7,10 @@ function DownloadAndRun($url, $file)
 
 if(-not $env:ChocolateyInstall -or -not (Test-Path "$env:ChocolateyInstall"))
 {
+	Write-Host "Chocolatey Not Found, Installing..."
 	iex ((new-object net.webclient).DownloadString('http://chocolatey.org/install.ps1')) 
 }
+
+Write-Host "Chocolatey Configured, Carry On!"
 
 Export-ModuleMember -Function @('DownloadAndRun')
--- Install-DotNetDev.ps1
+++ Install-DotNetDev.ps1
@@ -2,16 +2,16 @@ Import-Module .\Functions
 
 ## Development Tools - .NET
 
+## Should figure out if I want the "whole" VS or just the express edition
 ## cinst VisualStudio2013ExpressWeb
-
 cinst VisualStudio2013Professional -InstallArguments "WebTools"
 
+## These will likely be taken care of by the Visual Studio installer...
 cinst DotNet4.0
-
 cinst DotNet4.5
 
+## WebPI is clutch for installing other tooling
 cinst webpi
-
 cinst webpicommandline
 
 ## IIS Configuration
