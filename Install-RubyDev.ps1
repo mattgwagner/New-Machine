d054d93a6d5c2255ec7002fc91ffe746513e32ea
Matt Wagner <mattgwagner@gmail.com>
2014-04-20 10:28:18 -0500

    Updated ruby dev setup

--- Install-RubyDev.ps1
+++ Install-RubyDev.ps1
@@ -4,12 +4,15 @@ Import-Module .\Functions
 
 cinst ruby
 
+## This will change based on the current ruby to be installed, but is required to use gem
+$env:PATH += ";C:\ruby193\bin"
+
 cinst ruby.devkit
 
-gem update --system
+&gem update --system
 
-gem install rake
+&gem install rake
 
-gem install bundler
+&gem install bundler
 
-gem install rails
+&gem install rails
