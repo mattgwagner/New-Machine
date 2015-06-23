fcc5d4033c429a2fc7691ece289d348f646f8ac4
Matt Wagner <mattgwagner@gmail.com>
2015-06-23 09:31:55 -0400

    Remove script to install plex via ninite
    They weren't supporting that publicly

--- Install-Plex.ps1
+++
@@ -1,9 +0,0 @@
-$ScriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
-
-& "$ScriptPath\PowerShellProfile\Microsoft.PowerShell_profile.ps1"
-
-choco install plexmediaserver
-
-## Filebot is probably optional, but great for handling renaming of tv episodes based on tvdb
-
-choco install filebot
