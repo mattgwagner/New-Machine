dd342f158b6f57ad1c4c3806f5a582be07ec82cf
Matt Wagner <mattgwagner@gmail.com>
2017-04-16 11:35:22 -0400

    Update Install-Common.ps1

--- Install-Common.ps1
+++ Install-Common.ps1
@@ -19,6 +19,48 @@ choco install adobereader
 choco install greenshot
 choco install grepwin
 
+## Common Developer Tools
+
+choco install baretail
+
+choco install rdcman
+
+# Console emulator, tabbed PowerShell ftw
+
+choco install conemu
+
+## choco install redis-64
+## choco install redis-desktop-manager
+
+## choco install virtualbox
+## choco install vagrant
+
+## choco install freeSSHD
+## choco install Tunnelier
+
+## choco install jenkins
+
+## choco install rabbitmq
+
+## choco install nginx
+
+## choco install mysql
+## choco install postgresql
+
+## Development Tools - Git
+
+choco install git.install
+
+choco install TortoiseGit
+
+choco install poshgit
+
+## Development Tools for .NET
+
+choco install visualstudio2017professional visualstudio2017-workload-netweb visualstudio2017-workload-azure visualstudio2017-workload-netcoretools -y
+
+choco install mssqlserver2014express mssqlservermanagementstudio2014express -y
+
 ## System Configuration
 Enable-MicrosoftUpdate
 Set-ExplorerOptions -showFileExtensions
@@ -28,9 +70,3 @@ Enable-RemoteDesktop
 Disable-InternetExplorerESC # Server OS only
 
 Install-WindowsUpdate -AcceptEula
-
-## Development Tools for .NET
-
-choco install visualstudio2017professional visualstudio2017-workload-netweb visualstudio2017-workload-azure visualstudio2017-workload-netcoretools -y
-
-choco install mssqlserver2014express mssqlservermanagementstudio2014express -y
