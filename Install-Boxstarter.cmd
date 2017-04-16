b08ce9a8862ad030f2a979df6768f924e70e6391
Matt Wagner <mattgwagner@gmail.com>
2017-04-16 11:57:22 -0400

    Update and rename Install-Boxstarter.cmd to Install-ViaBoxstarter.cmd

--- Install-Boxstarter.cmd
+++
@@ -1,1 +0,0 @@
-START http://boxstarter.org/package/nr/url?https://raw.githubusercontent.com/mattgwagner/New-Machine/master/Install-Common.ps1
---
+++ Install-ViaBoxstarter.cmd
@@ -0,0 +1,3 @@
+rem Copy this into the run/console to invoke installation via BoxStarter
+
+START http://boxstarter.org/package/nr/url?https://raw.githubusercontent.com/mattgwagner/New-Machine/master/Install-Common.ps1
