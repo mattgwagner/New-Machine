672c7ab8f92162430b6dbd5f60569c6b7b44571f
Matt Wagner <mattgwagner@gmail.com>
2017-04-16 11:34:15 -0400

    Update Install-Common.ps1

--- Install-Common.ps1
+++ Install-Common.ps1
@@ -1,6 +1,8 @@
 ## This configuration file uses BoxStarter to configure the system
 ## and Chocolatey to install necessary packages
 
+choco install boxstarter
+
 ## .NET Frameworks -- Some should already be installed
 choco install DotNet3.5
 choco install DotNet4.5.1
@@ -26,3 +28,9 @@ Enable-RemoteDesktop
 Disable-InternetExplorerESC # Server OS only
 
 Install-WindowsUpdate -AcceptEula
+
+## Development Tools for .NET
+
+choco install visualstudio2017professional visualstudio2017-workload-netweb visualstudio2017-workload-azure visualstudio2017-workload-netcoretools -y
+
+choco install mssqlserver2014express mssqlservermanagementstudio2014express -y
