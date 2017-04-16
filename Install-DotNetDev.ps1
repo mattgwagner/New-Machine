f68422920ae4a527f57419526c5966a848e27d0d
Matt Wagner <mattgwagner@gmail.com>
2017-04-16 11:36:56 -0400

    Delete Install-DotNetDev.ps1

--- Install-DotNetDev.ps1
+++
@@ -1,5 +0,0 @@
-## Development Tools for .NET
-
-choco install visualstudio2017professional visualstudio2017-workload-netweb visualstudio2017-workload-azure visualstudio2017-workload-netcoretools -y
-
-choco install mssqlserver2014express mssqlservermanagementstudio2014express -y
