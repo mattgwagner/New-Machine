3315e325375ac60c79eba6789c0672fd43199e6d
Matt Wagner <mattgwagner@gmail.com>
2017-05-15 20:50:48 -0400

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,7 +1,7 @@
 # iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/mattgwagner/New-Machine/master/bootstrap.ps1'))
 
 $installScript = 'https://raw.githubusercontent.com/mattgwagner/New-Machine/master/Install-Common.ps1'
-$webLauncherUrl = "http://boxstarter.org/package/nr/url?$installScript"
+$webLauncherUrl = "http://boxstarter.org/package/url?$installScript"
 
 $IE=new-object -com internetexplorer.application
 $IE.navigate2($webLauncherUrl)
