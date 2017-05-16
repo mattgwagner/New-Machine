bea9562eb85b11cc83b095d8a687b6a5e33c7173
Matt Wagner <mattgwagner@gmail.com>
2017-05-15 20:58:02 -0400

    Delete bootstrap.ps1

--- bootstrap.ps1
+++
@@ -1,8 +0,0 @@
-# iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/mattgwagner/New-Machine/master/bootstrap.ps1'))
-
-$installScript = 'https://raw.githubusercontent.com/mattgwagner/New-Machine/master/Install-Common.ps1'
-$webLauncherUrl = "http://boxstarter.org/package/url?$installScript"
-
-$IE=new-object -com internetexplorer.application
-$IE.navigate2($webLauncherUrl)
-$IE.visible=$true
