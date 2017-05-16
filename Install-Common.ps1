fb6fb7128b2e5684df8dec85844173e8f46d46d2
Matt Wagner <mattgwagner@gmail.com>
2017-05-15 20:30:21 -0400

    Update Install-Common.ps1

--- Install-Common.ps1
+++ Install-Common.ps1
@@ -17,10 +17,12 @@ Enable-MicrosoftUpdate
 Install-WindowsUpdate -AcceptEula
 
 ## Web Browsers
+
 choco install GoogleChrome
 choco install Firefox
 
 ## Common System Tools
+
 choco install ccleaner 
 choco install 7zip.install 
 choco install notepadplusplus.install
@@ -30,9 +32,6 @@ choco install rdcman
 # Console emulator, tabbed PowerShell ftw
 
 choco install conemu
-
-## choco install redis-64
-## choco install redis-desktop-manager
 
 ## choco install jenkins
 
@@ -50,6 +49,8 @@ choco install TortoiseGit
 
 ## Development Tools - .NET
 
+choco install visualstudiocode
+
 choco install visualstudio2017professional 
 choco install visualstudio2017-workload-netweb 
 choco install visualstudio2017-workload-azure 
@@ -57,3 +58,8 @@ choco install visualstudio2017-workload-netcoretools
 
 choco install mssqlserver2014express-defaultinstance 
 choco install mssqlservermanagementstudio2014express
+
+## Development Tools - General
+
+choco install redis-64
+choco install redis-desktop-manager
