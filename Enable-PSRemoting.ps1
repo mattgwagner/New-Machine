7040bdf6696c9e712af2ab16053dcd23fb7ba281
Matt Wagner <mattgwagner@gmail.com>
2018-01-26 22:56:27 -0500

    Didn't like the old naming

--- Enable-PSRemoting.ps1
+++ Enable-PSRemoting.ps1
@@ -3,4 +3,3 @@ winrm set winrm/config/winrs @{MaxMemoryPerShellMB="512"}
 winrm set winrm/config @{MaxTimeoutms="1800000"}
 winrm set winrm/config/service @{AllowUnencrypted="true"}
 winrm set winrm/config/service/auth @{Basic="true"}
-sc config WinRM start= auto
