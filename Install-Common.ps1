e401646a2c037cc61915f0975772bacf9d910a36
Matt Wagner <mattgwagner@gmail.com>
2018-01-28 11:15:42 -0500

    No VStudio

--- Install-Common.ps1
+++ Install-Common.ps1
@@ -48,13 +48,13 @@ choco install TortoiseGit
 
 choco install visualstudiocode
 
-choco install visualstudio2017professional 
-choco install visualstudio2017-workload-netweb 
-choco install visualstudio2017-workload-azure 
-choco install visualstudio2017-workload-netcoretools
+# choco install visualstudio2017professional 
+# choco install visualstudio2017-workload-netweb 
+# choco install visualstudio2017-workload-azure 
+# choco install visualstudio2017-workload-netcoretools
 
-choco install sql-server-express
-choco install sql-server-management-studio
+# choco install sql-server-express
+# choco install sql-server-management-studio
 
 ## Development Tools - General
 
