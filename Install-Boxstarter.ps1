a0d489bf85bb345dc30c834a0d93a783805e73c7
Matt Wagner <mattgwagner@gmail.com>
2018-01-27 20:13:51 -0500

    Move some items into unattend

--- Install-Boxstarter.ps1
+++ Install-Boxstarter.ps1
@@ -1,2 +1,15 @@
 Invoke-Expression ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/mwrock/boxstarter/master/BuildScripts/bootstrapper.ps1'))
 Get-Boxstarter -Force
+
+## System Configuration
+
+# Set-ExplorerOptions -showFileExtensions
+
+Enable-RemoteDesktop
+Set-NetFirewallRule -Name RemoteDesktop-UserMode-In-TCP -Enabled True
+
+Disable-InternetExplorerESC # Server OS only
+
+Enable-MicrosoftUpdate
+# Disable-MicrosoftUpdate
+# Install-WindowsUpdate -AcceptEula
