1e0655e2dd3119381207291134bfe5a4cf7ad752
Matt Wagner <mattgwagner@gmail.com>
2018-02-05 21:17:09 -0500

    Not sure what the right order it

--- Sysprep.bat
+++ Sysprep.bat
@@ -1,3 +1,3 @@
 netsh advfirewall firewall set rule name="WinRM-HTTP" new action=block
 
-c:\windows\system32\sysprep\sysprep.exe /oobe /generalize /shutdown /mode:vm /unattend:a:\sysprep.xml /quiet
+c:\windows\system32\sysprep\sysprep.exe /oobe /generalize /shutdown /mode:vm /unattend:a:\autounattend.xml /quiet
--- Sysprep.ps1
+++
@@ -1,1 +0,0 @@
-c:\windows\system32\sysprep\sysprep.exe /oobe /generalize /shutdown /mode:vm
