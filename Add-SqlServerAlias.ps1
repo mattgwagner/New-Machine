32d1f84f13792f36dfbc3c0913a81efd9784d332
Matt Wagner <MattGWagner@gmail.com>
2018-02-14 11:44:59 -0500

    Tweak sql alias

--- Add-SqlServerAlias.ps1
+++ Add-SqlServerAlias.ps1
@@ -1,7 +1,8 @@
-# Set the two variables and then run in a PowerShell prompt
-$SQLAliasName = "SQLALIASNAME"
-
-$sqlserver = "SQLservername\instancename,1433" # Update as required. Change the port number if using a static custom port. If using a dynamic custom port, then remove the comma and port number - ,1433.
+param(
+    $Alias = ".\ss2012",
+    # Update as required. Change the port number if using a static custom port. If using a dynamic custom port, then remove the comma and port number - ,1433.
+    $Server = "localhost\sqlexpress"
+)
  
 # This script creates a 64bit and 32bit SQL Alias on the server. Run this script on all servers in your SharePoint farm.
 
@@ -9,7 +10,7 @@ Write-Host “Creating x64 SQL Alias”
 
 New-Item -path HKLM:SOFTWARE\Microsoft\MSSQLServer\Client\ConnectTo
 
-New-ItemProperty HKLM:SOFTWARE\Microsoft\MSSQLServer\Client\ConnectTo -name $SQLAliasName -propertytype String -value "DBMSSOCN,$sqlserver"
+New-ItemProperty HKLM:SOFTWARE\Microsoft\MSSQLServer\Client\ConnectTo -name $Alias -propertytype String -value "DBMSSOCN,$Server"
 
 Write-Host “Creating 32bit SQL Alias”
 
@@ -17,4 +18,4 @@ New-Item -path HKLM:SOFTWARE\Wow6432Node\Microsoft\MSSQLServer\Client\ConnectTo
 
 Write-Host "Configured SQL Alias on the Server"
 
-New-ItemProperty HKLM:SOFTWARE\Wow6432Node\Microsoft\MSSQLServer\Client\ConnectTo -name $SQLAliasName -propertytype String -value "DBMSSOCN,$sqlserver"
+New-ItemProperty HKLM:SOFTWARE\Wow6432Node\Microsoft\MSSQLServer\Client\ConnectTo -name $Alias -propertytype String -value "DBMSSOCN,$Server"
