915714deb5861505403ea5345631300b9a2bbabc
Matt Wagner <MattGWagner@gmail.com>
2018-05-21 20:03:31 -0400

    Eject volume media hash change

--- Install-VirtualBoxGuestAdditions.ps1
+++ Install-VirtualBoxGuestAdditions.ps1
@@ -15,12 +15,11 @@ if ($p.ExitCode) {
 }
 
 Write-Host 'Ejecting the VirtualBox Guest Additions media...'
+
 $ejectVolumeMediaExeUrl = 'https://github.com/rgl/EjectVolumeMedia/releases/download/v1.0.0/EjectVolumeMedia.exe'
-$ejectVolumeMediaExeHash = 'f7863394085e1b3c5aa999808b012fba577b4a027804ea292abf7962e5467ba0'
+
 $ejectVolumeMediaExe = "$env:TEMP\EjectVolumeMedia.exe"
+
 Invoke-WebRequest $ejectVolumeMediaExeUrl -OutFile $ejectVolumeMediaExe
-$ejectVolumeMediaExeActualHash = (Get-FileHash $ejectVolumeMediaExe -Algorithm SHA256).Hash
-if ($ejectVolumeMediaExeActualHash -ne $ejectVolumeMediaExeHash) {
-    throw "the $ejectVolumeMediaExeUrl file hash $ejectVolumeMediaExeActualHash does not match the expected $ejectVolumeMediaExeHash"
-}
+
 &$ejectVolumeMediaExe E
