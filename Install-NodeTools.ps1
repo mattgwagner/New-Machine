5e5522ca106abd319057fedfaf8e47c21b04c0da
Matt Wagner <mattgwagner@gmail.com>
2018-08-23 15:56:37 -0400

    Update Install-NodeTools.ps1

--- Install-NodeTools.ps1
+++ Install-NodeTools.ps1
@@ -1,9 +1,10 @@
 ## This configuration file uses BoxStarter to configure the system
 ## and Chocolatey to install necessary packages
 
-## Development Tools - Node.JS
+## Development Tools - Node.JS & Yarn
 
 choco install nodejs
+choco install yarn
 
 ## Development Tools - Mongo
 
