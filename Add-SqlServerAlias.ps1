579e92ca296a6420d63ffa222d7ad5f57684589c
Matt Wagner <mattgwagner@gmail.com>
2018-09-17 23:01:25 -0400

    Fixing backtick in add alias

--- Add-SqlServerAlias.ps1
+++ Add-SqlServerAlias.ps1
@@ -6,13 +6,13 @@ param(
  
 # This script creates a 64bit and 32bit SQL Alias on the server. Run this script on all servers in your SharePoint farm.
 
-Write-Host “Creating x64 SQL Alias”
+Write-Host "Creating x64 SQL Alias"
 
 New-Item -path HKLM:SOFTWARE\Microsoft\MSSQLServer\Client\ConnectTo
 
 New-ItemProperty HKLM:SOFTWARE\Microsoft\MSSQLServer\Client\ConnectTo -name $Alias -propertytype String -value "DBMSSOCN,$Server"
 
-Write-Host “Creating 32bit SQL Alias”
+Write-Host "Creating 32bit SQL Alias"
 
 New-Item -path HKLM:SOFTWARE\Wow6432Node\Microsoft\MSSQLServer\Client\ConnectTo
 
