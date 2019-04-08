2499f1fd3b83e73280ad30c59f00ab14cb5ab30b
Matt Wagner <mattgwagner@gmail.com>
2019-04-07 21:33:46 -0400

    Create Install-DevTools.ps1

---
+++ Install-DevTools.ps1
@@ -0,0 +1,17 @@
+## Development Tools - General
+
+choco install postman
+choco install baretail
+
+chocol install vscode
+
+choco install conemu
+
+# choco install redis-64
+# choco install redis-desktop-manager
+
+## choco install rabbitmq
+
+## choco install nginx
+
+## choco install postgresql
