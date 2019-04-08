2428c053ffd3b12c5f74b5d4e19f88ce1bc21275
Matt Wagner <mattgwagner@gmail.com>
2019-04-07 21:33:44 -0400

    Update Install-Common.ps1

--- Install-Common.ps1
+++ Install-Common.ps1
@@ -38,21 +38,3 @@ choco install rdcman
 # choco install google-backup-and-sync
 
 # choco install adobereader
-
-## Development Tools - General
-
-choco install postman
-choco install baretail
-
-chocol install vscode
-
-choco install conemu
-
-# choco install redis-64
-# choco install redis-desktop-manager
-
-## choco install rabbitmq
-
-## choco install nginx
-
-## choco install postgresql
