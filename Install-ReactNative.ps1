a93502d91416ba2e4f83580470886fc9af6b7ff9
Matt Wagner <mattgwagner@gmail.com>
2019-04-08 21:37:09 -0400

    Update Install-ReactNative.ps1

--- Install-ReactNative.ps1
+++ Install-ReactNative.ps1
@@ -2,6 +2,10 @@ choco install android-sdk
 
 choco install androidstudio /AddToDesktop
 
+## This is also installed via Install-NodeTools.ps1
+
+choco install yarn
+
 yarn add global expo-cli
 
 yarn add global react-native-cli
