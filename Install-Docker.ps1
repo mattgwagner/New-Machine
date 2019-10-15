77d2ec69fcf54b34e35d0ae016a0dac4c90bbb08
Matt Wagner <mattgwagner@gmail.com>
2019-10-15 15:22:30 -0400

    Update Install-Docker.ps1

--- Install-Docker.ps1
+++ Install-Docker.ps1
@@ -1,5 +1,7 @@
-choco install docker
+# choco install docker
 
-choco install docker-compose
+# choco install docker-compose
+
+# Docker Desktop include the CLI and compose
 
 choco install docker-desktop
