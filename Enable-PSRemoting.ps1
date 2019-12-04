899a969f96bc202835beb9a859f82585c451310e
Matt Wagner <mattgwagner@matts-mbp.lan>
2019-12-04 09:37:10 -0500

    tweak ps removing config

--- Enable-PSRemoting.ps1
+++ Enable-PSRemoting.ps1
@@ -24,6 +24,23 @@ if (!(New-Object System.Security.Principal.WindowsPrincipal(
     throw 'this must run with Administrator privileges (e.g. in a elevated shell session)'
 }
 
-# configure WinRM.
-winrm set winrm/config/service '@{AllowUnencrypted="true"}'
-winrm set winrm/config/service/auth '@{Basic="true"}'
+# Supress network location Prompt
+New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Network\NewNetworkWindowOff" -Force
+
+# Set network to private
+$ifaceinfo = Get-NetConnectionProfile
+Set-NetConnectionProfile -InterfaceIndex $ifaceinfo.InterfaceIndex -NetworkCategory Private 
+
+# Set up WinRM and configure some things
+winrm quickconfig -q
+winrm s "winrm/config" '@{MaxTimeoutms="1800000"}'
+winrm s "winrm/config/winrs" '@{MaxMemoryPerShellMB="2048"}'
+winrm s "winrm/config/service" '@{AllowUnencrypted="true"}'
+winrm s "winrm/config/service/auth" '@{Basic="true"}'
+
+# Enable the WinRM Firewall rule, which will likely already be enabled due to the 'winrm quickconfig' command above
+Enable-NetFirewallRule -DisplayName "Windows Remote Management (HTTP-In)"
+
+sc.exe config winrm start= auto
+
+exit 0
