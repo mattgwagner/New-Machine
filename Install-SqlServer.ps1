e7c03aa2cdbdd5b45378323b410675b7fadcce00
Matt Wagner <mattgwagner@gmail.com>
2020-02-12 13:23:33 -0500

    Update Install-SqlServer.ps1

--- Install-SqlServer.ps1
+++ Install-SqlServer.ps1
@@ -1,5 +1,7 @@
 Write-Output "Starting SQL Server Installs"
 
 choco install sql-server-express -ia "/IACCEPTSQLSERVERLICENSETERMS /Q /ACTION=install /INSTANCEID=MSSQLSERVER /INSTANCENAME=MSSQLSERVER /UPDATEENABLED=FALSE" -o -y
+
 choco install sql-server-management-studio
-# choco install sql-operations-studio
+
+choco install azure-data-studio
