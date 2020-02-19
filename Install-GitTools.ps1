4c3a51caed3a40f90b324b06b10aa15e3746a05b
Matt Wagner <mattgwagner@gmail.com>
2020-02-18 20:23:26 -0500

    Update Install-GitTools.ps1

--- Install-GitTools.ps1
+++ Install-GitTools.ps1
@@ -3,8 +3,9 @@
 choco install git --package-parameters= "/GitAndUnixToolsOnPath /WindowsTerminal /NoShellIntegration /NoGuiHereIntegration /NoShellHereIntegration"
 choco install TortoiseGit
 choco install github-desktop
-choco install poshgit
 
 mkdir "$($env:USERPROFILE)\.ssh" # Create .ssh folder for keys
 
 Get-Service -Name ssh-agent | Set-Service -StartupType Manual
+
+PowerShellGet\Install-Module posh-git -Scope CurrentUser -AllowPrerelease -Force
