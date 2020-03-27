8cc6c47a00ff68d34279cfe362d55a3d5a90b239
unknown <MattGWagner@Gmail.com>
2020-03-27 15:40:55 -0400

    Tweaks to build script

--- Build-HyperV.ps1
+++ Build-HyperV.ps1
@@ -1,16 +1,14 @@
 $here = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-& ".\Install-Chocolatey.ps1"
-
-if(!(Test-Path $env:ProgramFiles\Oracle\VirtualBox)) { cinst virtualbox -y }
+& "$here\Install-Chocolatey.ps1"
 
 if(!(Get-Command vagrant -ErrorAction SilentlyContinue)) { cinst vagrant -y }
 if(!(Get-Command packer -ErrorAction SilentlyContinue)) { cinst packer -y }
 
 $Timer = [Diagnostics.Stopwatch]::StartNew()
 
-packer build windows-2019.json
+packer build --only=hyperv-iso windows-2019.json 
 
 $Timer.Stop()
 
-Write-Host "VirtualBox Image Built in $($Timer.Elapsed)"
+Write-Host "Hyper-V Image Built in $($Timer.Elapsed)"
