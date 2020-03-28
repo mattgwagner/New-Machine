4b0d60172ff6576e7fd90c936adda0b355892209
unknown <MattGWagner@Gmail.com>
2020-03-28 18:58:55 -0400

    Fix script

--- Update-GitSubtree.ps1
+++ Update-GitSubtree.ps1
@@ -1,4 +1,4 @@
 # git subtree add --prefix .github-actions https://github.com/actions/virtual-environments.git master --squash
 
-git subtree pull --prefix .github-actions https://github.com/actions/virtual-environments.git master
+git subtree pull --prefix .github-actions https://github.com/actions/virtual-environments.git master --squash
 
