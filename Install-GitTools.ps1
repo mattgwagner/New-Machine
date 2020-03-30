8cf55fa195becde4de0ca895ea703a4c8e38fef5
unknown <MattGWagner@Gmail.com>
2020-03-30 14:32:09 -0400

    Add more logging

--- Install-GitTools.ps1
+++ Install-GitTools.ps1
@@ -5,12 +5,14 @@ choco install github-desktop
 
 mkdir "$($env:USERPROFILE)\.ssh" # Create .ssh folder for keys
 
-# Get-Service -Name ssh-agent | Set-Service -StartupType Manual
+Get-Service -Name ssh-agent | Set-Service -StartupType Manual
 
 Write-Output "Installing PoshGit"
 
-Install-Module posh-git -Scope CurrentUser -Force
+Install-Module posh-git -Scope CurrentUser -Force -AllowClobber
 
 RefreshEnv
+
+Write-Output "Installing PoshGit to Profile"
 
 Add-PoshGitToProfile
