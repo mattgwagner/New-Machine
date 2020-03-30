e692fb707aee0ab2ffed0e0d8bd53021e1aa9408
Matt Wagner <mattgwagner@gmail.com>
2020-03-29 21:23:06 -0400

    Tweak to install posh-git

--- Install-GitTools.ps1
+++ Install-GitTools.ps1
@@ -5,6 +5,10 @@ choco install github-desktop
 
 mkdir "$($env:USERPROFILE)\.ssh" # Create .ssh folder for keys
 
-# Get-Service -Name ssh-agent | Set-Service -StartupType Manual
+Get-Service -Name ssh-agent | Set-Service -StartupType Manual
 
-# PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
+PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
+
+RefreshEnv
+
+Add-PoshGitToProfile
