916997102394df5ed85b481e5e3a3cd474fd9f59
unknown <MattGWagner@Gmail.com>
2020-03-30 09:04:56 -0400

    Don't try and start the ssh-agent?

--- Install-GitTools.ps1
+++ Install-GitTools.ps1
@@ -5,7 +5,7 @@ choco install github-desktop
 
 mkdir "$($env:USERPROFILE)\.ssh" # Create .ssh folder for keys
 
-Get-Service -Name ssh-agent | Set-Service -StartupType Manual
+# Get-Service -Name ssh-agent | Set-Service -StartupType Manual
 
 PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
 
