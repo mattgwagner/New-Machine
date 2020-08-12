c999d8583f678fe41861c07bac7554ae4fd30c21
Matt Wagner <mattgwagner@matts-mac-mini.lan>
2020-08-11 22:58:55 -0400

    Tweak win2019 templates for new paths

---
+++ Windows-2019/Build-Minimal.ps1
@@ -0,0 +1,9 @@
+$here = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$Timer = [Diagnostics.Stopwatch]::StartNew()
+
+packer build --only=hyperv-iso $here\windows-2019-minimal.json
+
+$Timer.Stop()
+
+Write-Host "Hyper-V Image Built in $($Timer.Elapsed)"
