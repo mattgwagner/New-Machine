5088132ce0e10178071ce3862ab1ede9e0dbe8e3
Matt Wagner <MattGWagner@Gmail.com>
2020-08-17 21:47:28 -0400

    Provide a flag to skip making it a vagrant box

--- Windows-2019/Build.ps1
+++ Windows-2019/Build.ps1
@@ -1,8 +1,19 @@
+param (
+    [bool]$DoNotVagrantIfy = $false
+)
+
 $here = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $Timer = [Diagnostics.Stopwatch]::StartNew()
 
-packer build --only=hyperv-iso $here\windows-2019.json
+if($DoNotVagrantIfy)
+{
+    packer build --only=hyperv-iso --except vagrant-image $here\windows-2019.json 
+}
+else
+{
+    packer build --only=hyperv-iso $here\windows-2019.json 
+}
 
 $Timer.Stop()
 
