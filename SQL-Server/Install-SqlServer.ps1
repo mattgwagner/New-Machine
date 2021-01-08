a1429eeed00a0a4c2f2f5af1221314042f3c58eb
Matt Wagner <Matt@RedLeg.dev>
2021-01-08 15:39:55 -0500

    Update Install-SqlServer.ps1

--- SQL-Server/Install-SqlServer.ps1
+++ SQL-Server/Install-SqlServer.ps1
@@ -1,6 +1,10 @@
 Write-Output "Starting SQL Server Installs"
 
-choco install sql-server-express -ia "/IACCEPTSQLSERVERLICENSETERMS /Q /ACTION=install /INSTANCEID=MSSQLSERVER /INSTANCENAME=MSSQLSERVER /UPDATEENABLED=FALSE" -o -y
+# choco install sql-server-express -ia "/IACCEPTSQLSERVERLICENSETERMS /Q /ACTION=install /INSTANCEID=MSSQLSERVER /INSTANCENAME=MSSQLSERVER /UPDATEENABLED=FALSE" -o -y
+
+# Switching to SQL Server 2019 Developer Edition, which is free for all non-prod uses and doesn't limit to 10gb dbs like Express
+
+choco install sql-server-2019
 
 choco install sql-server-management-studio
 
