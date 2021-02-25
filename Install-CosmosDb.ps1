974d4d64ad849958226518ebcb0218e6288b7388
Matt Wagner <Matt@RedLeg.dev>
2021-02-25 11:30:21 -0500

    Add CosmosDb tooling to dev install

---
+++ Install-CosmosDb.ps1
@@ -0,0 +1,12 @@
+
+# Emulator for local development
+
+choco install azure-cosmosdb-emulator
+
+# For viewing db contents locally, easily
+
+choco install choco install cosmosdbexplorer
+
+# For migrating data between systems
+
+choco install azure-documentdb-data-migration-tool
