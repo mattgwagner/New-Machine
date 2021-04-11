5063f059a2bfb1e333ba0d2d9420ba9e3316b811
Matt Wagner <Matt@RedLeg.dev>
2021-04-11 11:07:53 -0400

    Cosmos DB Explorer isn't actually that great

--- Install-CosmosDb.ps1
+++ Install-CosmosDb.ps1
@@ -3,10 +3,6 @@
 
 choco install azure-cosmosdb-emulator
 
-# For viewing db contents locally, easily
-
-choco install choco install cosmosdbexplorer
-
 # For migrating data between systems
 
 choco install azure-documentdb-data-migration-tool
