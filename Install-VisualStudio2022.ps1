1d662b3f5b34af807939e595f9950fd5f6d89039
Matt Wagner <Matt@RedLeg.dev>
2021-09-22 09:59:05 -0400

    Skip optional

--- Install-VisualStudio2022.ps1
+++ Install-VisualStudio2022.ps1
@@ -1,3 +1,3 @@
 # TODO: Update this once it's live release
 
-choco install visualstudio2022community-preview --pre --package-parameters "--allWorkloads --includeRecommended --includeOptional --passive"
+choco install visualstudio2022community-preview --pre --package-parameters "--allWorkloads --includeRecommended --passive"
