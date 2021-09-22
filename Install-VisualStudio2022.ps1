a556a34bd99d63854cd246642a8e4d324b80adb0
Matt Wagner <Matt@RedLeg.dev>
2021-09-22 09:58:43 -0400

    Add VS 2022

---
+++ Install-VisualStudio2022.ps1
@@ -0,0 +1,3 @@
+# TODO: Update this once it's live release
+
+choco install visualstudio2022community-preview --pre --package-parameters "--allWorkloads --includeRecommended --includeOptional --passive"
