d7b9da7a6b6f6b67b1c741495b0f1f624b8526b1
Matt Wagner <Matt@RedLeg.dev>
2021-11-13 14:16:11 -0500

    VS2022 RTW

--- Install-VisualStudio2022.ps1
+++ Install-VisualStudio2022.ps1
@@ -1,3 +1,3 @@
 # TODO: Update this once it's live release
 
-choco install visualstudio2022community-preview --pre --package-parameters "--allWorkloads --includeRecommended --passive"
+choco install visualstudio2022community --package-parameters "--allWorkloads --includeRecommended --passive"
