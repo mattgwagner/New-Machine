3fd3cfb5df7e6b4af08c400500467f7be8bdc6e4
mattgwagner <matt@redleg.dev>
2025-01-01 12:38:39 -0500

    Updates to macOS

--- MacOS/Setup.sh
+++ MacOS/Setup.sh
@@ -13,3 +13,17 @@ defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
 # Disable "natural" scroll
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+brew tap "homebrew/bundle"
+
+brew install "git"
+
+brew install "flux"
+
+brew install "google-backup-and-sync"
+
+brew install "keepingyouawake"
+
+brew install "rectangle"
+
+brew install "powershell"
